0a4c7809dad49de63e866d5ccf30b0949d1c77cc
Nikita Brylev <nbrylevv@gmail.com>
2020-11-11 23:31:08 +0300

    Update world-initializer.sh

--- world-initializer.sh
+++ world-initializer.sh
@@ -8,25 +8,36 @@ apt update
 apt install curl -y
 apt install xclip -y
 
+# Gnome stuff
+apt-get install gnome-tweak-tool -y
+apt install gnome-shell-extensions -y
+apt-get install dconf-tools -y
+
+# Flatpack
+apt install flatpak -y
+apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+
 #
 apt install git -y
 apt install zsh -y
-apt install docker -y
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
+#apt install docker -y
 apt install nodejs -y
 
 #
 #apt install wine-stable -y
-apt install snapd -y
-snap install webstorm --classic
-snap install sublime-text --classic
-snap install telegram-desktop --classic
+#apt install snapd -y
+#snap install webstorm --classic
+#snap install sublime-text --classic
+#snap install telegram-desktop --classic
 #
-snap install spotify
+#snap install spotify
 
 # Done. Now folders
 
 [ ! -d $DOCUMENTS_DIR ] mkdir $DOCUMENTS_DIR
-mkdir $DOCUMENTS_DIR/Projects
+mkdir $DOCUMENTS_DIR/Pet
 mkdir $DOCUMENTS_DIR/Work
 
-#Bye :)
+# That's all, folks!
